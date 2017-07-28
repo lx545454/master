3fb0b9c0d8362f8da309c017a26e0c724c830d29
lx545454 <1906322800@qq.com>
2017-07-28 10:47:29 +0800

    s

--- app/Http/Controllers/PayController.php
+++ app/Http/Controllers/PayController.php
@@ -13,6 +13,7 @@ class PayController extends Controller
     public function recharge(Request $request)
     {
         $sub_data['user_token'] = $request->input('user_token', '');
+        $sub_data['merchant_id'] = $request->input('merchant_id', '');
         $sub_data['amount'] = $request->input('amount', '');
         $sub = REQ::requset_all('pay/'.__FUNCTION__,'form',$sub_data);
         return $sub;
