c150c6e9d3e146df52c380316f7d3c129e3d1bc8
lx545454 <1906322800@qq.com>
2017-08-22 09:48:03 +0800

    s

--- app/Http/Controllers/SwooleController.php
+++ app/Http/Controllers/SwooleController.php
@@ -14,6 +14,7 @@ class SwooleController extends Controller
 
     public function index()
     {
+        phpinfo();
         //创建websocket服务器对象，监听0.0.0.0:9502端口
         $ws = new \swoole_server("0.0.0.0", 9502);
 
