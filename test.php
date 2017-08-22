0289b835f0c7afd1a89c0ce1f092fd7d164b6a4e
lx545454 <1906322800@qq.com>
2017-08-22 10:43:59 +0800

    s

---
+++ test.php
@@ -0,0 +1,18 @@
+<?php
+$serv = new Swoole\Websocket\Server("127.0.0.1", 9502);
+
+$serv->on('Open', function($server, $req) {
+    echo "connection open1: ".$req->fd;
+});
+
+$serv->on('Message', function($server, $frame) {
+    echo "message: ".$frame->data;
+    $server->push($frame->fd, "sssddd");
+});
+
+$serv->on('Close', function($server, $fd) {
+    echo "connection close: ".$fd;
+});
+
+$serv->start();
+?>
