c9afea30b7dd469667cef6ea9c838a07b1039413
lx545454 <1906322800@qq.com>
2017-12-26 19:45:20 +0800

    b

--- app/Http/Controllers/SscController.php
+++ app/Http/Controllers/SscController.php
@@ -1000,7 +1000,7 @@ class SscController extends Controller
     }
 
     public function get_qicis($request=[]){
-        $ssc = DB::table('game_ssc')->where('num','<>',"0")->orderBy('id','desc')->get()->toArray();
+        $ssc = DB::table('game_ssc')->where('number','<>',"0")->orderBy('id','desc')->get()->toArray();
         if($ssc){
             return H::renderJson($ssc);
         }else{
